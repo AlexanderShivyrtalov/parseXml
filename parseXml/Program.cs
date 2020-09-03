600dc1923d636a745e003679997c5431da3b95d0
shivi <shivi@DESKTOP-0Q3LMGQ>
2020-09-03 16:28:24 +0400

    Добавьте файлы проекта.

---
+++ parseXml/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Xml;
+
+namespace parseXml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load("http://www.cbr.ru/scripts/XML_daily.asp");
+            string nominal = "";
+            string name = "";
+            string value = "";
+
+            XmlElement xRoot = xDoc.DocumentElement;
+            foreach (XmlNode xnode in xRoot)
+            {
+                if (xnode.Attributes.Count > 0)
+                {
+                    XmlNode attr = xnode.Attributes.GetNamedItem("ID");
+                    if (attr.Value == "R01200")
+                    {
+                        Console.WriteLine(attr.Value);
+                        foreach (XmlNode childnode in xnode.ChildNodes)
+                        {
+                            if (childnode.Name == "Name")
+                            {
+                                name = childnode.InnerText;
+                                //Console.WriteLine($"Валюта: {childnode.InnerText}");
+                            }
+
+                            if (childnode.Name == "Nominal")
+                            {
+                                nominal = childnode.InnerText;
+                                //Console.WriteLine($"Курс: {childnode.InnerText}");
+                            }
+
+                            if (childnode.Name == "Value")
+                            {
+                                value = childnode.InnerText;
+                                //Console.WriteLine($"Курс: {childnode.InnerText}");
+                            }
+                        }
+                    }
+                }
+            }
+            Console.WriteLine($"Курс {nominal} {name} равен {value} рублям");
+            Console.Read();
+        }
+    }
+}
---
+++ parseXml/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("parseXml")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("parseXml")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("516eff70-b115-4c2d-925d-53340b6dfa86")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
